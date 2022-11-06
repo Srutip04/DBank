2fa6ae9c81675a7cb6c19680da90b21809e394fd
Srutip04 <subuhishruti75@gmail.com>
2022-11-06 21:03:50 +0530

    modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,14 @@
 import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
+
 import "./App.css";
 
 function App() {
   return (
-    <EthProvider>
-      <div id="App" >
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
+    <div id="App">
+      <div className="container">
+        <h1>Hello World</h1>
       </div>
-    </EthProvider>
+    </div>
   );
 }
 
