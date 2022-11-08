3e8331971f8af9078ad76bb3cb0ef3b248350aa0
Srutip04 <subuhishruti75@gmail.com>
2022-11-08 21:48:35 +0530

    transfer func

--- truffle/contracts/Tether.sol
+++ truffle/contracts/Tether.sol
@@ -7,5 +7,32 @@ contract Tether{
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8   public decimals = 18;
 
+    event Transfer(
+      address indexed _from,
+      address indexed _to,
+      uint _value
+    );
 
+    event Approve(
+      address indexed _owner,
+      address indexed _spender,
+      uint _value
+    );
+
+    mapping(address => uint) public balanceOf;
+
+    constructor(){
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _from,address _to, uint _value) public returns(bool success){
+        //require that the value is greater or equal for transfer
+        require(balanceOf[msg.sender] >= _value);
+        //transfer the amt & subtract the balance
+        balanceOf[msg.sender] -= _value;
+        //add the balance
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
