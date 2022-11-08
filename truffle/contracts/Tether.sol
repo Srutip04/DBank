de523a4f52ead875c0d7d6f2b0d64e79d4a2cea2
Srutip04 <subuhishruti75@gmail.com>
2022-11-08 21:30:44 +0530

    tether smart contract shell

---
+++ truffle/contracts/Tether.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Tether{
+    string  public name = "Mock Tether Token";
+    string  public symbol = "mUSDT";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8   public decimals = 18;
+
+
+}
