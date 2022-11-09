750423cafb8cf88dcd52b653e176506f282d4abb
Srutip04 <subuhishruti75@gmail.com>
2022-11-10 05:26:33 +0530

    Mocha & Chai

---
+++ truffle/test/decentralBank.js
@@ -0,0 +1,11 @@
+const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract ('decentralBank' , accounts =>{
+    
+})
