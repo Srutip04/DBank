a40f867e8ebe03448024acd87dbbe28d6e14ab85
Srutip04 <subuhishruti75@gmail.com>
2022-11-10 04:03:04 +0530

    transfer

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -4,14 +4,33 @@ const RWD  = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
 
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer,network,accounts) {
   // Deploy Mock Tether Token
   // await deployer.deploy(Tether);
   // const tether = await Tether.deployed();
 
- deployer.deploy(SimpleStorage);
- deployer.deploy(Tether);
- deployer.deploy(RWD);
- deployer.deploy(DecentralBank);
+ await deployer.deploy(SimpleStorage);
+ 
+ //deploy mock tether contract
+ await deployer.deploy(Tether);
+ const tether = await Tether.deployed();
+ 
+ //deploy rwd contract
+ await deployer.deploy(RWD);
+ const rwd = await RWD.deployed();
+ 
+ //deploy decentralbamk contract
+ await deployer.deploy(DecentralBank);
+ const decentralBank = await DecentralBank.deployed();
+
+ //Transfer all RWD tokens to decentral bank
+ await rwd.transfer(decentralBank.address, "1000000000000000000000000");
+
+ //distribute 100 tether tokens to investor
+ await tether.transfer(accounts[1], "1000000000000000000");
+
+
+
+
   
 };
