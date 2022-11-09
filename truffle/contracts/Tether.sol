ee1962604a98f31ef55fdb1cd7c8bdc074d79ec9
Srutip04 <subuhishruti75@gmail.com>
2022-11-09 19:30:15 +0530

    transfer & transferFrom

--- truffle/contracts/Tether.sol
+++ truffle/contracts/Tether.sol
@@ -13,25 +13,45 @@ contract Tether{
       uint _value
     );
 
-    event Approve(
+    event Approval(
       address indexed _owner,
       address indexed _spender,
       uint _value
     );
 
     mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
 
     constructor(){
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _from,address _to, uint _value) public returns(bool success){
+    function transfer(address _to, uint _value) public returns(bool success){
         //require that the value is greater or equal for transfer
         require(balanceOf[msg.sender] >= _value);
         //transfer the amt & subtract the balance
         balanceOf[msg.sender] -= _value;
         //add the balance
         balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender,uint _value) public returns (bool success){
+       allowance[msg.sender][_spender] = _value;
+       emit Approval(msg.sender, _spender, _value);
+       return true;
+    }
+    
+    //third parties to run transactions
+    function transferFrom(address _from,address _to,uint256 _value) public returns(bool success) {
+       require(_value <= balanceOf[_from]);
+       require(_value <= allowance[_from][msg.sender]);
+       //add the balance
+        balanceOf[_to] += _value;
+        //subtract the balance
+        balanceOf[_from] -= _value;
+        allowance[msg.sender][_from] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
