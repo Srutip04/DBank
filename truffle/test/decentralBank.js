711397c8ad396b58da47b117623172c0e7cb0135
Srutip04 <subuhishruti75@gmail.com>
2022-11-10 12:57:24 +0530

    assertions

--- truffle/test/decentralBank.js
+++ truffle/test/decentralBank.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
@@ -6,6 +8,21 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract ('decentralBank' , accounts =>{
+contract('DecentralBank' , (accounts) =>{
+    describe('Mock Tether Deployment',async () =>{
+        it('matches name successfully', async () =>{
+            let tether = await Tether.new()
+            const name = await tether.name()
+            assert.equal(name, "Mock Tether Token");
+        })
+    })
     
+     describe("Reward Token", async () => {
+       it("matches name successfully", async () => {
+         let reward = await RWD.new();
+         const name = await reward.name();
+         assert.equal(name, "Reward Token");
+       });
+     });
+
 })
