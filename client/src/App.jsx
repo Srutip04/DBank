6fc44c04b3a539700dc9dceed34796259db78ea6
Srutip04 <subuhishruti75@gmail.com>
2022-11-12 12:55:39 +0530

    helloooo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { EthProvider } from "./contexts/EthContext";
 
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div id="App">
-      <div className="container">
+      <div className="text-center">
         <h1>Hello World</h1>
       </div>
     </div>
